717abbeebde1f7eb84a3a23abde22f5323144f80
zcbe <benyahia.zaim@gmail.com>
2024-08-18 13:31:39 +0200

    add back-end

---
+++ chatop/src/main/java/zcbe/chatop/ChatopApplication.java
@@ -0,0 +1,13 @@
+package zcbe.chatop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatopApplication.class, args);
+	}
+
+}
---
+++ chatop/src/test/java/zcbe/chatop/ChatopApplicationTests.java
@@ -0,0 +1,13 @@
+package zcbe.chatop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
